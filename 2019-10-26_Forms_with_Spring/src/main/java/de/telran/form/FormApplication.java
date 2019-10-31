ff455ebfcff368b81d3903a7b07681b51631acd5
ASas77 <alezi77@rambler.ru>
2019-10-31 20:48:31 +0100

    add my Name

--- 2019-10-26_Forms_with_Spring/src/main/java/de/telran/form/FormApplication.java
+++ 2019-10-26_Forms_with_Spring/src/main/java/de/telran/form/FormApplication.java
@@ -3,7 +3,7 @@ package de.telran.form;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication /// Alesia , Test pull Request
 public class FormApplication {
 
     public static void main(String[] args) {
