e8bae720940186880fb6a870094b68380672903c
NikolajSankovDev <nikolaj.sankov.dev@gmail.com>
2019-10-31 20:57:17 +0100

    CW_2019-10-31

--- 2019-10-26_Forms_with_Spring/src/main/java/de/telran/form/FormApplication.java
+++ 2019-10-26_Forms_with_Spring/src/main/java/de/telran/form/FormApplication.java
@@ -9,6 +9,5 @@ public class FormApplication {
     public static void main(String[] args) {
         SpringApplication.run(FormApplication.class, args);
     }
-
-    /////////
+        //Nikolaj
 }
