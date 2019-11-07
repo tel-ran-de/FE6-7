e56e06e4d77ca1ed7b94f4c649f8bb2dd3c9293a
Alex Loginov <alexander.loginov@takeaway.com>
2019-11-07 21:55:47 +0100

    Validating form fields

--- 2019-10-26_Forms_with_Spring/src/main/resources/static/js/add-post.js
+++ 2019-10-26_Forms_with_Spring/src/main/resources/static/js/add-post.js
@@ -1,27 +1,62 @@
-$(document).ready(function() {
-    $('button.btn.btn-primary').click(
+$(function() {
+
+    $('button.btn.btn-primary').on('click',
         function (event) {
             event.preventDefault();
             console.log(event);
         }
     );
 
-     $("small#titleHelp").hide();
+    const isEmptyValidator = function ($input) {
+        console.log($input.val());
+        if ($input.val().length === 0)
+            return {
+                valid: false,
+                errorMessage: 'This field is required'
+            };
+        return {
+            valid: true
+        };
+    };
 
-     $ ('input#title.form-control').on({
-         focus: function(){
-             $("small#titleHelp").show();
-         },
-         blur: function(){
-             $("small#titleHelp").hide();
-         },
-         input : function(){
-             $("small#titleHelp").show();
-         },
+    const isNotMoreThanValidator = function ($input) {
+        if ($input.val().length > 50)
+            return {
+                valid: false,
+                errorMessage: "This field can't be more then 50 symbols"
+            };
+        return {
+            valid: true
+        };
+    };
 
+    const $inputTitle = $('#title');
+    $inputTitle.on({
+        focus: function(){
+            $inputTitle.removeClass('is-invalid');
+        },
+        blur: function(){
+            const validators = [isEmptyValidator, isNotMoreThanValidator];
+            for (let validator of validators) {
+                let error = validator($inputTitle);
+                if (!error.valid) {
+                    $inputTitle.addClass('is-invalid');
+                    $inputTitle.removeClass('is-valid');
+                    $inputTitle.siblings('.invalid-feedback').html(error.errorMessage);
+                    return;
+                }
+            }
+            $inputTitle.addClass('is-valid');
+            $inputTitle.removeClass('is-invalid');
+        },
+        input : function(){
+            $("#titleHelp").show();
+        },
         click: function(){
-            alert(" be careful ");
+            //alert("be careful");
         }
     });
+
+
 });
 
