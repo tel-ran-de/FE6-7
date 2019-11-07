824eb676a3e1258c4192c038e4b2f9332b045147
Ira Baker <49585685+paxpistor@users.noreply.github.com>
2019-11-07 11:24:44 +0100

    Create add-post.js

---
+++ 2019-10-26_Forms_with_Spring/src/main/resources/static/add-post.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('button.btn.btn-primary').click(
+        function (event) {
+            event.preventDefault();
+            console.log(event);
+        });
+});
