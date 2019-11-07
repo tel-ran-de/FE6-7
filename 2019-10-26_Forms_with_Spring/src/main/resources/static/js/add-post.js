c9fb853724ddcbe461965f8e1873663637c1a00a
dumitru <sonik4ever85@gmail.com>
2019-11-07 02:57:23 +0100

    HW 2019-11-05

--- 2019-10-26_Forms_with_Spring/src/main/resources/static/js/add-post.js
+++ 2019-10-26_Forms_with_Spring/src/main/resources/static/js/add-post.js
@@ -8,3 +8,31 @@ $(document).ready(function() {
 });
 
 
+// FIRST TASK
+$(document).ready(function() {
+    $('#title').click(
+        function () {
+            alert("take care you are typing the title")
+        }
+    );
+});
+
+//SECOND TASK
+
+$(document).ready(function(){
+    $('#titleHelp').hide();
+});
+$(document).ready(function() {
+    $('#title').keypress(function () {
+            $('#titleHelp').show()
+        }
+    );
+});
+$(document).ready(function() {
+    $('#title').keydown(function () {
+            $('#titleHelp').hide()
+        }
+    );
+});
+
+
