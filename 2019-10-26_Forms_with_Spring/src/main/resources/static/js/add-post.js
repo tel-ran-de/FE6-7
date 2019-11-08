c4128820386659e83900836fe5b88088b86e5cbe
x3matt <x3matt21@mail.ru>
2019-11-08 23:13:59 +0700

    HomeWork 2019_11_07

--- 2019-10-26_Forms_with_Spring/src/main/resources/static/js/add-post.js
+++ 2019-10-26_Forms_with_Spring/src/main/resources/static/js/add-post.js
@@ -1,11 +1,54 @@
 $(function() {
 
+    $("#titleHelp").hide();
+    $("#postHelp").hide();
+
     $('button.btn.btn-primary').on('click',
         function (event) {
             event.preventDefault();
             console.log(event);
         }
     );
+
+    const $inputTitle = $('#title');
+    const $inputBody = $('#textarea');
+
+
+    function activateValidation ($input) {
+        $input.on({
+            focus: function () {
+                $input.removeClass('is-invalid');
+            },
+            blur: function () {
+                const validators = [isEmptyValidator, isNotMoreThanValidator];
+                for (let validator of validators) {
+                    let error = validator($input);
+                    if (!error.valid) {
+                        $input.addClass('is-invalid');
+                        $input.removeClass('is-valid');
+                        $input.siblings('.invalid-feedback').html(error.errorMessage);
+                        return;
+                    }
+                }
+                $input.addClass('is-valid');
+                $input.removeClass('is-invalid');
+            },
+            input : function(){
+                if($input.attr('id') === 'title') $("#titleHelp").show();
+                if($input.attr('id') === 'textarea') $("#postHelp").show();
+                },
+            click: function(){
+                // alert("be careful");
+            }
+        });
+    }
+
+    function start() {
+        const inputs = [$inputTitle,$inputBody];
+        for(let input of inputs){activateValidation(input)}
+    }
+
+    start();
 
     const isEmptyValidator = function ($input) {
         console.log($input.val());
@@ -20,42 +63,26 @@ $(function() {
     };
 
     const isNotMoreThanValidator = function ($input) {
-        if ($input.val().length > 50)
+        if($input.attr('id') === 'textarea'){
+            if ($input.val().length > 4096)
+                return {
+                    valid: false,
+                    errorMessage: "This field can't be more then 4096 symbols"
+                };
             return {
-                valid: false,
-                errorMessage: "This field can't be more then 50 symbols"
+                valid: true
             };
-        return {
-            valid: true
-        };
-    };
-
-    const $inputTitle = $('#title');
-    $inputTitle.on({
-        focus: function(){
-            $inputTitle.removeClass('is-invalid');
-        },
-        blur: function(){
-            const validators = [isEmptyValidator, isNotMoreThanValidator];
-            for (let validator of validators) {
-                let error = validator($inputTitle);
-                if (!error.valid) {
-                    $inputTitle.addClass('is-invalid');
-                    $inputTitle.removeClass('is-valid');
-                    $inputTitle.siblings('.invalid-feedback').html(error.errorMessage);
-                    return;
-                }
-            }
-            $inputTitle.addClass('is-valid');
-            $inputTitle.removeClass('is-invalid');
-        },
-        input : function(){
-            $("#titleHelp").show();
-        },
-        click: function(){
-            //alert("be careful");
+        }else {
+            if ($input.val().length > 50)
+                return {
+                    valid: false,
+                    errorMessage: "This field can't be more then 50 symbols"
+                };
+            return {
+                valid: true
+            };
         }
-    });
+    };
 
 
 });
