7a57fed368d12dd2c8e291f256cdc8ba292b2431
Alex Loginov <alexander.loginov@takeaway.com>
2019-11-12 19:33:17 +0100

    Generalizing form validation

---
+++ 2019-10-26_Forms_with_Spring/src/main/resources/static/js/forms.js
@@ -0,0 +1,68 @@
+const markInputValid = function ($input) {
+    $input.addClass('is-valid');
+    $input.removeClass('is-invalid');
+};
+
+const markInputInvalid = function ($input, errorMessage) {
+    $input.addClass('is-invalid');
+    $input.removeClass('is-valid');
+    $input.siblings('.invalid-feedback').html(errorMessage);
+};
+
+const markInputPristine = function($input) {
+    $input.removeClass('is-invalid');
+};
+
+$('button.btn.btn-primary').on('click',
+    function (event) {
+        event.preventDefault();
+        console.log(event);
+    }
+);
+
+const isEmptyValidator = function ($input) {
+    console.log($input.val());
+    if ($input.val().length === 0)
+        return {
+            valid: false,
+            errorMessage: 'This field is required'
+        };
+    return {
+        valid: true
+    };
+};
+
+const createIsNotMoreThanValidator = function(maxLength) {
+    return function ($input) {
+        if ($input.val().length > maxLength)
+            return {
+                valid: false,
+                errorMessage: "This field can't be more then " + maxLength + " symbols"
+            };
+        return {
+            valid: true
+        };
+    };
+};
+
+let validateForm = function(form) {
+    Object.keys(form.inputs).forEach(key => {
+        const $input = $('form#'+ form.id +' [name=' + key + ']');
+        const validators = form.inputs[key];
+        $input.on({
+            focus: function(){
+                markInputPristine($input);
+            },
+            blur: function(){
+                for (let validator of validators) {
+                    let error = validator($input);
+                    if (!error.valid) {
+                        markInputInvalid($input, error.errorMessage);
+                        return;
+                    }
+                }
+                markInputValid($input);
+            }
+        });
+    });
+};
