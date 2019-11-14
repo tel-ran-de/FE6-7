b0d67a4233ef717df79649caec4e26a5398ab064
Evgen <evgen.skyba@gmail.com>
2019-11-14 12:41:53 +0100

    HW 2019-11-12

--- 2019-10-26_Forms_with_Spring/src/main/resources/static/js/add-post.js
+++ 2019-10-26_Forms_with_Spring/src/main/resources/static/js/add-post.js
@@ -1,12 +1,15 @@
 $(function() {
     const isNotMoreThan50Validator = createIsNotMoreThanValidator(50);
     const isNotMoreThan4096Validator = createIsNotMoreThanValidator(4096);
+    const isNotMoreThan25ValidatorAuthorFirstName = createIsNotMoreThanValidator(25);
 
     const addPostForm = {
         id: 'addNewPostForm',
         inputs: {
             title: [isEmptyValidator, isNotMoreThan50Validator],
-            postBody: [isEmptyValidator, isNotMoreThan4096Validator]
+            postBody: [isEmptyValidator, isNotMoreThan4096Validator],
+            first_name: [isEmptyValidator, isNotMoreThan25ValidatorAuthorFirstName],
+            last_name: [isEmptyValidator, isNotMoreThan50Validator]
         }
     };
 
