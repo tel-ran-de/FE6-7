0b44768ef92716f1adad84162f00747dc8daf15e
Alex Loginov <alexander.loginov@takeaway.com>
2019-11-14 20:24:22 +0100

    add-author form js part

---
+++ 2019-10-26_Forms_with_Spring/src/main/resources/static/js/add-author.js
@@ -0,0 +1,14 @@
+$(function() {
+    const isNotMoreThan25Validator = createIsNotMoreThanValidator(25);
+    const isNotMoreThan50Validator = createIsNotMoreThanValidator(50);
+
+    const addAuthorForm = {
+        id: 'addNewAuthorForm',
+        inputs: {
+            firstName: [isEmptyValidator, isNotMoreThan25Validator],
+            lastName: [isEmptyValidator, isNotMoreThan50Validator]
+        }
+    };
+
+    validateForm(addAuthorForm);
+});
