e2fb1411d7dcf48dd7ae7453b97ff09c17d4dd06
Alex Loginov <alexander.loginov@takeaway.com>
2019-11-16 12:23:39 +0100

    Author controller - accept json

--- 2019-10-26_Forms_with_Spring/src/main/java/de/telran/form/AuthorController.java
+++ 2019-10-26_Forms_with_Spring/src/main/java/de/telran/form/AuthorController.java
@@ -1,21 +1,26 @@
 package de.telran.form;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.telran.form.dto.AuthorDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/author")
 @Slf4j
 public class AuthorController {
+
     @PostMapping
-    public String createNewPost(@RequestParam("firstName") String firstName,
-                                @RequestParam("lastName") String lastName){
+    public String createNewPost(@RequestBody AuthorDto author){
         log.info(
                 "Request received. First name = {}, Last name = {}",
-                firstName,
-                lastName
+                author.getFirstName(),
+                author.getLastName()
         );
         return "New post created";
     }
---
+++ 2019-10-26_Forms_with_Spring/src/main/java/de/telran/form/dto/AuthorDto.java
@@ -0,0 +1,9 @@
+package de.telran.form.dto;
+
+import lombok.*;
+
+@Data
+public class AuthorDto {
+    private String firstName;
+    private String lastName;
+}
