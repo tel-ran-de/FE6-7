22a72db0e3b814804fb475f43380be73000627d3
Vladislav <x3matt21@mail.ru>
2019-11-16 17:22:54 +0300

    HW_2019-11-16

---
+++ 2019-10-26_Forms_with_Spring/src/main/java/de/telran/form/dto/PostDto.java
@@ -0,0 +1,11 @@
+package de.telran.form.dto;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+    private String title;
+    private String postBody;
+    private String date;
+    private String author;
+}
