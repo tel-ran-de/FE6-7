7887b19b95f1393d2463d3824b08379e58907328
JanKelsch <kelsh@bigmir.net>
2019-11-17 18:17:34 +0100

    Sending data as JSON and accepted JSON in PostController

---
+++ 2019-10-26_Forms_with_Spring/src/main/java/de/telran/form/dto/PostDto.java
@@ -0,0 +1,11 @@
+package de.telran.form.dto;
+
+import lombok.*;
+
+@Data
+public class PostDto {
+    private String title;
+    private String body;
+    private String date;
+    private String author;
+}
