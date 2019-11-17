ff84eee4bc89a541e066031c5b2040ff992c0a8c
venividiviciregina <venividiviciregina@gmail.com>
2019-11-17 04:45:35 +0100

    HW_2019_11_16 1.add post form: send data to BE as JSON 2.Modify controller method to accept JSON

---
+++ 2019-10-26_Forms_with_Spring/src/main/java/de/telran/form/dto/PostDto.java
@@ -0,0 +1,12 @@
+package de.telran.form.dto;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+    private String title;
+    private String postBody;
+    private String date;
+    private String author;
+
+}
