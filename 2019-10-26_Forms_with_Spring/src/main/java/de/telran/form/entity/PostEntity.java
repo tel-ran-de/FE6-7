e6b6cf9b38023462bad45a8e191e79da8a6106a0
Alex Loginov <alexander.loginov@takeaway.com>
2019-11-19 21:19:13 +0100

    Spring Data JPA - configuration of DB

--- 2019-10-26_Forms_with_Spring/src/main/java/de/telran/form/entity/PostEntity.java
+++ 2019-10-26_Forms_with_Spring/src/main/java/de/telran/form/entity/PostEntity.java
@@ -2,13 +2,18 @@ package de.telran.form.entity;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
+@Entity
 public class PostEntity {
+    @Id
+    @GeneratedValue
     private Long id;
     private String title;
     private String body;
     private Date date;
+    @ManyToOne(targetEntity = AuthorEntity.class)
     private AuthorEntity authorEntity;
 }
