af52d97e0ed20fa74cb777329075c5d5f3dc2cf9
Evgen <evgen.skyba@gmail.com>
2019-11-19 15:52:19 +0100

    HW 2019-11-16

---
+++ 2019-10-26_Forms_with_Spring/src/main/java/de/telran/form/dto/PostDto.java
@@ -0,0 +1,42 @@
+package de.telran.form.dto;
+
+import java.util.Date;
+
+public class PostDto {
+    private String title;
+    private String text;
+    private Date date;
+    private String author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
--- 2019-10-26_Forms_with_Spring/src/main/resources/static/js/add-post.js
+++ 2019-10-26_Forms_with_Spring/src/main/resources/static/js/add-post.js
@@ -15,23 +15,25 @@ $(function() {
 
     $('form#'+ formId + ' button[type=submit]').on('click',
         function(event) {
-        event.preventDefault();
-        console.log(event);
-        const formData = $('form#' + formId).serialize();
-        console.log(formData);
-
-        $.ajax({
-            url: '/post',
-            type: 'POST',
-            data: formData,
-            success: function(response) {
-                console.log(response);
-                $('form div.alert-success').html(response);
-                $('form div.alert-success').show();
-            },
-            error: function(error) {
-                $('form div.alert-danger').html(error.responseText);
-                $('form div.alert-danger').show();
+            event.preventDefault();
+            console.log(event);
+            const formData = $('form#' + formId).serializeArray();
+            console.log(formData);
+            const formDataJson = convertToJson(formData);
+            console.log(formDataJson);
+            $.ajax({
+                url: '/post',
+                type: 'POST',
+                data: JSON.stringify(formDataJson),
+                contentType: "application/json",
+                success: function(response) {
+                    console.log(response);
+                    $('form div.alert-success').html(response);
+                    $('form div.alert-success').show();
+                },
+                error: function(error) {
+                    $('form div.alert-danger').html(error.responseText);
+                    $('form div.alert-danger').show();
             }
         });
     });
