7770116c791be07a2a4aeff2dc32c67b6d4600e2
Vladislav <x3matt21@mail.ru>
2019-11-28 01:05:43 +0300

    added solution

--- 2019-11-23_Angular/my-blog/src/app/navigation/navigation.component.ts
+++ 2019-11-23_Angular/my-blog/src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NavBarDto} from "../dto/NavBarDto";
 
 @Component({
   selector: 'app-navigation',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
+
+
+  navBars: NavBarDto[] = [
+    {name: 'Home' , link: 'https://www.google.ru/'},
+    {name: 'about' , link: 'https://www.google.ru/'},
+    {name: 'Sample Post' , link: 'https://www.google.ru/'},
+    {name: 'Contact' , link: 'https://www.google.ru/'}
+    ];
 
   constructor() { }
 
