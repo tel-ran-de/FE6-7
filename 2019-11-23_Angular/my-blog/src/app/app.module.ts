12edf70dfab7b562d63a9d91675c2bdabe3845f3
sonik4ever85 <sonik4ever85@gmail.com>
2019-11-27 22:20:21 +0100

    .

--- 2019-11-23_Angular/my-blog/src/app/app.module.ts
+++ 2019-11-23_Angular/my-blog/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -18,7 +19,8 @@ import { PostPreviewComponent } from './post-preview/post-preview.component';
     PostPreviewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
