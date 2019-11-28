563545e9b54f77aca22c956ebd586696e037f846
ASas77 <alezi77@rambler.ru>
2019-11-28 21:22:39 +0100

    add contact

--- 2019-11-23_Angular/my-blog/src/app/app-routing.module.ts
+++ 2019-11-23_Angular/my-blog/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AboutComponent} from './component/about/about.component';
 import {MainComponent} from './component/main/main.component';
+import {ContactComponent} from './component/contact/contact.component';
 
 
 const routes: Routes = [
   { path: '', component: MainComponent},
   { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
