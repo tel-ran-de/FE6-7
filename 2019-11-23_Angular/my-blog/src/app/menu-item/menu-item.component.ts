4cf24433104983c3468b3e93cea45f0fe4e2d7fd
Evgen <evgen.skyba@gmail.com>
2019-11-28 14:09:45 +0100

    HW_2019_11_26

---
+++ 2019-11-23_Angular/my-blog/src/app/menu-item/menu-item.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-menu-item',
+  templateUrl: './menu-item.component.html',
+  styleUrls: ['./menu-item.component.scss']
+})
+export class MenuItemComponent implements OnInit {
+
+  @Input()
+  menu: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
