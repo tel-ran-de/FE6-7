d6691a2f816c6b5545412b6a12dcdae228143a64
venividiviciregina <venividiviciregina@gmail.com>
2019-12-12 13:16:59 +0100

    Homework  for HW 2019-12-11

--- 2019-11-23_Angular/my-blog/src/app/app-routing.module.ts
+++ 2019-11-23_Angular/my-blog/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './component/about/about.component';
 import { MainComponent } from './component/main/main.component';
 import {PostComponent} from './component/post/post.component';
+import {CreatePostComponent} from './component/create-post/create-post.component';
 
 
 const routes: Routes = [
   { path: '', component: MainComponent},
   { path: 'about', component: AboutComponent },
-  { path: 'post/:id', component: PostComponent }
+  { path: 'post/:id', component: PostComponent },
+  { path: 'create-post', component: CreatePostComponent}
 ];
 
 @NgModule({
