0de2cb0ac3f94c48fcf0535677175b80f6c6ba2b
Arkarine <karine.arustamyan@gmail.com>
2019-12-13 19:35:08 +0100

    delete comment

--- 2019-11-23_Angular/my-blog/src/app/service/post/post.service.ts
+++ 2019-11-23_Angular/my-blog/src/app/service/post/post.service.ts
@@ -10,36 +10,6 @@ export class PostService {
 
   private URL_POST = '/post';
 
-  // posts: PostDto[] = [
-  //   {
-  //     id: 1,
-  //     title: 'My First Post in this blog',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla feugiat iaculis tellus, vitae vehicula arcu aliquet non. Sed a efficitur augue, quis elementum erat. Suspendisse sodales, sapien id placerat imperdiet, augue lectus efficitur nulla, vitae hendrerit eros metus a ligula. Nullam nisl lorem, tincidunt a enim non, dapibus semper nunc. Donec consequat nibh est, sodales fermentum ex consequat a. Suspendisse potenti. Aenean vel pulvinar nunc, eget dapibus augue. Ut vitae pretium arcu. Aenean sed justo justo. Mauris mollis vel justo non scelerisque. Etiam vel ex non nisi cursus auctor id at magna.',
-  //     date: '2019-12-10'
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'My Second Post in this blog',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla feugiat iaculis tellus, vitae vehicula arcu aliquet non. Sed a efficitur augue, quis elementum erat. Suspendisse sodales, sapien id placerat imperdiet, augue lectus efficitur nulla, vitae hendrerit eros metus a ligula. Nullam nisl lorem, tincidunt a enim non, dapibus semper nunc. Donec consequat nibh est, sodales fermentum ex consequat a. Suspendisse potenti. Aenean vel pulvinar nunc, eget dapibus augue. Ut vitae pretium arcu. Aenean sed justo justo. Mauris mollis vel justo non scelerisque. Etiam vel ex non nisi cursus auctor id at magna.',
-  //     date: '2019-12-10'
-  //   },
-  //   {
-  //     id: 3,
-  //     title: 'My Third Post in this blog',
-  //     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla feugiat iaculis tellus, vitae vehicula arcu aliquet non. Sed a efficitur augue, quis elementum erat. Suspendisse sodales, sapien id placerat imperdiet, augue lectus efficitur nulla, vitae hendrerit eros metus a ligula. Nullam nisl lorem, tincidunt a enim non, dapibus semper nunc. Donec consequat nibh est, sodales fermentum ex consequat a. Suspendisse potenti. Aenean vel pulvinar nunc, eget dapibus augue. Ut vitae pretium arcu. Aenean sed justo justo. Mauris mollis vel justo non scelerisque. Etiam vel ex non nisi cursus auctor id at magna.',
-  //     date: '2019-12-10'
-  //   }
-  // ];
-
-  /*getPost(id: number): PostDto {
-  for (const post of this.posts) {
-    if (post.id === id) {
-      return post;
-    }
-  }
-  return null;
-  }*/
-
   constructor(private http: HttpClient) { }
 
   getPost(id: number): Observable<PostDto> {
