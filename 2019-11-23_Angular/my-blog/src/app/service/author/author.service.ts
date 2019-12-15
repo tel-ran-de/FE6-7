49ce3d590b9a7e53329394e76d3e774e72815be0
venividiviciregina <venividiviciregina@gmail.com>
2019-12-15 22:31:42 +0100

    Homework HW 2019-12-14 - Correct 1. Add functionality to add Author from Angular to DB

--- 2019-11-23_Angular/my-blog/src/app/service/author/author.service.ts
+++ 2019-11-23_Angular/my-blog/src/app/service/author/author.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {AuthorDto} from '../../dto/AuthorDto';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,16 @@ import {AuthorDto} from '../../dto/AuthorDto';
 export class AuthorService {
 
   private URL_AUTHOR = '/api/author';
+  private URL_POST: string;
 
   constructor(private http: HttpClient) { }
+  getAuthor(id: number): Observable<AuthorDto> {
+    return this.http.get<AuthorDto>(this.URL_POST + `/${id}`);
+  }
+
+  getAllAuthors(): Observable<AuthorDto[]> {
+    return this.http.get<AuthorDto[]>(this.URL_POST);
+  }
 
   saveAuthor(authorDto: AuthorDto) {
     return this.http.post(this.URL_AUTHOR, authorDto);
